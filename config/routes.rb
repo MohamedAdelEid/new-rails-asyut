4c47fb31ce859d009ece023567be93a4c7824459
MohamedAdelEid <dev.mohamedadell@gmail.com>
2026-06-16 19:36:03 +0300

    Add Post CRUD operations and custom route

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :posts, except: [:create]
+  post "posts/commit", to: "posts#create", as: :create_post
 end
